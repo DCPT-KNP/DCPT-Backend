b1c054f7bb2f4b208dae66fbb006f283924b7223
jiho5993 <jiho5993@naver.com>
2022-05-02 12:00:50 +0900

    deploy: add test server script

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
+# ./deploy.sh prod : 서버 배포
+# ./deploy.sh test : 테스트 서버 배포
+# ./deploy.sh : ec2에서 컨테이너 구동
 
-if [ "$1" = "local" ]; then
+# TODO: 도커 스크립트 작성
+
+if [ "$1" = "prod" ]; then
+
+    echo "******************** Deploy real server... ********************"
     # remove image
-    echo "******************** Remove previous image... ********************"
     docker rmi -f jiho5993/nerdit
 
     # new-build/re-build docker image
-    echo "******************** Build new image... ********************"
     docker-compose build
 
     # push image
-    echo "******************** Push image... ********************"
     docker push jiho5993/nerdit
+    echo "******************** Success ********************"
+
+elif [ "$1" = "test" ]; then
+
+    # test server deploy
+    echo "******************** Deploy test server... ********************"
+    docker rmi -f jiho5993/nerdit-test
+    docker-compose -f docker-compose.test.yml build
+    docker push jiho5993/nerdit-test
+    echo "******************** Success ********************"
+
 else
     # remove container
-    echo "******************** Remove previous container... ********************"
-    docker rm -f app
+    docker kill $(docker ps -q)
 
     # pull image
-    echo "******************** Pull image... ********************"
     docker pull jiho5993/nerdit
 
     # run container
-    echo "******************** Run container... ********************"
-    docerk-compose up -d
-
-    # prune image
-    echo "******************** Prune previous image... ********************"
-    docker image prune -af
+    docerk-compose up -d jiho5993/nerdit
 fi
